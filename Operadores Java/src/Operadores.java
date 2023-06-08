3132692452f19b26dab6aa59d85d6bf6979a6e72
MMSemedo <103007636+MMSemedo@users.noreply.github.com>
2023-06-08 01:44:04 -0300

    foi mostrado o uso de operadores unarios.

--- Operadores Java/src/Operadores.java
+++ Operadores Java/src/Operadores.java
@@ -1,37 +1,65 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
+        boolean variavel = true;
 
-        // Uso de concatenação simples {String nomeCompleto = "Linguagem" + "Java";}
+        variavel = !variavel;
 
-        //String nomeCompleto = "Linguagem" + "Java";
-        //System.out.println(nomeCompleto);
-
-        String Concatenacao = "?";
-        
-        System.out.println(Concatenacao);
-
-        Concatenacao = 1 + 1 + 1 + "1";
-        
-        System.out.println(Concatenacao);
-
-        Concatenacao = 1 + "1" + 1 + 1;
-
-        System.out.println(Concatenacao);
+        System.out.println(variavel);
 
 
-        Concatenacao = 1 + "1" + 1 + "1";
-
-        System.out.println(Concatenacao);
-
-
-        Concatenacao = "1" + 1 + 1 + 1;
-
-        System.out.println(Concatenacao);
-
-
-        Concatenacao = "1" + (1 + 1 + 1);
-
-        System.out.println(Concatenacao);
-        
     }
 }
+
+
+
+        /*int numero = 5;
+        // x repetição
+        
+        System.out.println(++numero);// para imprimir com o novo valor poder ser usar o ++ como prefixo
+        *
+    }
+
+                                } /*int numero = 5;
+  
+  numero = -numero; //aqui foi usado o unario negativo para transformar o numero positvo em negativo.
+  
+  System.out.println(numero);*/
+
+                          //numero = numero * -1; /*para converter o número negativo em positivo multiplica-se por 1
+                          //Ex: numero = numero* -1*/
+                          
+                          
+                          // Uso de concatenação simples {String nomeCompleto = "Linguagem" + "Java";}
+                          
+                          //String nomeCompleto = "Linguagem" + "Java";
+                          //System.out.println(nomeCompleto);
+                          
+                          /*String Concatenacao = "?";
+                          
+                          System.out.println(Concatenacao);
+                          
+                          Concatenacao = 1 + 1 + 1 + "1";
+                          
+                          System.out.println(Concatenacao);
+                          
+                          Concatenacao = 1 + "1" + 1 + 1;
+                          
+                          System.out.println(Concatenacao);
+                          
+                          
+                          Concatenacao = 1 + "1" + 1 + "1";
+                          
+                          System.out.println(Concatenacao);
+                          
+                          
+                          Concatenacao = "1" + 1 + 1 + 1;
+                          
+                          System.out.println(Concatenacao);
+                          
+                          
+                          Concatenacao = "1" + (1 + 1 + 1);
+                          
+                          System.out.println(Concatenacao);
+                          */
+
+    
